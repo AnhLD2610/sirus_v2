ce1073cf80443b145323806dd71513f36b07e022
AnhLD2610 <leducanh26102002@gmail.com>
2025-05-28 10:15:28 +0700

    push

--- Bert/train.py
+++ Bert/train.py
@@ -315,51 +315,36 @@ class Manager(object):
 
                 # loss4 = self.moment.distillation_loss_att(attention_des_pre, attention_des , 10) + self.moment.distillation_loss_att(attention_pre, attention, 10)
              
-<<<<<<< HEAD
                 # hidden_pre = F.normalize(hidden_pre, dim=1)  
                 # rep_des_pre = F.normalize(rep_des_pre, dim=1) 
                 # sim  = F.cosine_similarity(hidden_pre, rep_des_pre, dim=1)  # [B]
                 # mask = (sim > 0.8)                                     # [B], torch.bool or byte tensor
                 # # mask = sim                               # [B], torch.bool or byte tensor
-=======
-                hidden_pre = F.normalize(hidden_pre, dim=1)  
-                rep_des_pre = F.normalize(rep_des_pre, dim=1) 
-                sim  = F.cosine_similarity(hidden_pre, rep_des_pre, dim=1)  # [B]
-                mask = (sim > 0.9)                                     # [B], torch.bool or byte tensor
->>>>>>> 823cf07533c58c6d988485f96ffdbbf33972ad59
 
-                loss_att = self.moment.distillation_loss_att(
-                    attention_teacher_layers=attention_pre, 
-                    attention_student_layers=attention, 
-                    top_k_val=args.top_k,
-                    mask=mask
-                )  
+                # loss_att = self.moment.distillation_loss_att(
+                #     attention_teacher_layers=attention_pre, 
+                #     attention_student_layers=attention, 
+                #     top_k_val=args.top_k,
+                #     mask=mask
+                # )  
 
-                loss_att_des = self.moment.distillation_loss_att(
-                    attention_teacher_layers=attention_des_pre, 
-                    attention_student_layers=attention_des, 
-                    top_k_val=args.top_k,
-                    mask=mask
-                )  
-                loss4 = loss_att + loss_att_des
+                # loss_att_des = self.moment.distillation_loss_att(
+                #     attention_teacher_layers=attention_des_pre, 
+                #     attention_student_layers=attention_des, 
+                #     top_k_val=args.top_k,
+                #     mask=mask
+                # )  
+                # loss4 = loss_att + loss_att_des
 
                 # loss_hid = self.moment.distillation_loss_hidden(
                 #         hidden_teacher = hidden_pre,
                 #         hidden_student = hidden,
-<<<<<<< HEAD
                 #         mask = mask
-=======
-                #         # mask = mask
->>>>>>> 823cf07533c58c6d988485f96ffdbbf33972ad59
                 # )
                 # loss_hid_des = self.moment.distillation_loss_hidden(
                 #     hidden_teacher = rep_des_pre,
                 #     hidden_student = rep_des,
-<<<<<<< HEAD
                 #     mask = mask
-=======
-                #     # mask = mask
->>>>>>> 823cf07533c58c6d988485f96ffdbbf33972ad59
                 # )
                 # loss4 = loss_hid + loss_hid_des
 
